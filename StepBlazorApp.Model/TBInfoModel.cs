23ef2dcf94eac62445f4a5a1d0493c0e084ad514
CBoy <wei_ying_yu@hotmail.com>
2020-07-19 07:50:42 +0800

    添加项目文件。

---
+++ StepBlazorApp.Model/TBInfoModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StepBlazorApp.Model
+{
+    public class TBInfoModel
+    {
+        //--------------------------------------------------------------------------
+        /// <summary>
+        /// dbName --- 数据库名称
+        /// </summary>
+        public string dbName { get; set; }
+        //--------------------------------------------------------------------------
+        /// <summary>
+        /// tbName --- 数据表名称
+        /// </summary>
+        public string tbName { get; set; }
+        //--------------------------------------------------------------------------
+        /// <summary>
+        /// tbInfo --- 数据表信息
+        /// </summary>
+        public string tbInfo { get; set; }
+        //--------------------------------------------------------------------------
+        /// <summary>
+        /// fdName --- 字段名称
+        /// </summary>
+        public string fdName { get; set; }
+        //--------------------------------------------------------------------------
+        /// <summary>
+        /// fdInfo --- 字段信息
+        /// </summary>
+        public string fdInfo { get; set; }
+        //--------------------------------------------------------------------------
+        /// <summary>
+        /// fdType --- 字段类型
+        /// </summary>
+        public string fdType { get; set; }
+        //--------------------------------------------------------------------------
+        /// <summary>
+        /// fdLength --- 字段长度
+        /// </summary>
+        public int fdLength { get; set; }
+        //--------------------------------------------------------------------------
+        /// <summary>
+        /// is_nullable --- 是否可空
+        /// </summary>
+        public bool is_nullable { get; set; }
+        //--------------------------------------------------------------------------
+    }
+}
---
+++ StepBlazorApp/Startup.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using StepBlazorApp.Data;
+
+namespace StepBlazorApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
+            services.AddBlazorise(options =>
+            {
+                options.ChangeTextOnKeyPress = true; // optional
+            }).AddBootstrapProviders().AddFontAwesomeIcons();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.ApplicationServices.UseBootstrapProviders().UseFontAwesomeIcons(); 
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+        }
+    }
+}
